570f0a75afa5dbff6f61cbddd01f29e98c904f25
MitchOlya <mitch-gay@yandex.ru>
2017-12-08 01:08:12 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app',
+  template: `<div class="bg-success p-1 text-xs-center">
+                This is SportsStore
+                </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
