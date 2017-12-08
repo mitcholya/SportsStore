02f142dd2879ca9bf04b15f136cccab39c14629e
MitchOlya <mitch-gay@yandex.ru>
2017-12-09 00:50:16 +0500

    working with store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: `<div class="bg-success p-1 text-xs-center">
-                This is SportsStore
-                </div>`,
+  template: "<store></store>",
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
