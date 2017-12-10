bd689452e2a714c5b77af886c1801c135689a4eb
MitchOlya <mitch-gay@yandex.ru>
2017-12-10 21:37:04 +0500

    working with rest api

--- src/app/model/static.datasource.ts
+++ src/app/model/static.datasource.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "@angular/core";
 import { Product } from "./product.model";
 import { Observable } from "rxjs/Observable";
+import { Order } from "./order.model";
 import "rxjs/add/observable/from";
 
 @Injectable()
 export class StaticDataSource {
+
     private products: Product[] = [
         new Product(1, "Product 1", "Category 1", "Product 1 (Category 1)", 100),
         new Product(2, "Product 2", "Category 1", "Product 2 (Category 1)", 100),
@@ -22,7 +24,14 @@ export class StaticDataSource {
         new Product(14, "Product 14", "Category 3", "Product 14 (Category 3)", 100),
         new Product(15, "Product 15", "Category 3", "Product 15 (Category 3)", 100),
     ];
+
     getProducts(): Observable<Product[]> {
         return Observable.from([this.products]);
     }
+
+    saveOrder(order: Order): Observable<Order> {
+        console.log(JSON.stringify(order));
+        return Observable.from([order]);
+    }
+
 }
